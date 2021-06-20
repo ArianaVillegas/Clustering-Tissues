8e584b07ff454ecf1929fd359c65014abfb2873d
ArianaVillegas <ariana.villegas@utec.edu.pe>
2021-06-20 12:59:35 -0500

    Organize files

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import numpy as np
 import pandas as pd
 
 from sklearn.decomposition import TruncatedSVD
-from sklearn.mixture import GaussianMixture
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-from clustering import dbscan, mean_shift, gmm, group_classes_2, k_means
 from src.dbscan import DBScan
 from src.gmm import GMM
 from src.k_means import KMeans
@@ -17,20 +13,9 @@ y = pd.read_csv('../dataset/clase.txt')
 svd = TruncatedSVD(n_components=20)
 df_svd = svd.fit_transform(df)
 
-# scaler = MinMaxScaler(feature_range=(0, 10))
-# df_svd = scaler.fit_transform(df_svd)
-
 classificator = GMM(k=7)
 classificator.fit(df_svd)
 classes, clusters = classificator.predict(df_svd).values()
-# classes, clusters = k_means(df_svd, k=7)
-# classes, clusters = mean_shift(df_svd, radio=100)
-# classes, clusters = dbscan(df_svd, radio=100, min_pts=4)
-# classes, clusters = gmm(df_svd, n_k=7, max_iter=5)
-# gm = KMeans(n_clusters=7, random_state=0, verbose=1).fit(df_svd)
-# gm = GaussianMixture(n_components=7, random_state=0, verbose=1).fit(df_svd)
-# labels = gm.predict(df_svd)
-# classes, clusters = group_classes_2(labels, df_svd)
 classes_names = []
 
 for c in classes:
--- src/utils.py
+++ src/utils.py
@@ -7,11 +7,3 @@ def multivariate_normal(x, mean, covariance):
     return (2 * np.pi) ** (-len(x) / 2) * np.linalg.det(covariance) ** (-1 / 2) * np.exp(
         -np.dot(np.dot((x - mean).T, np.linalg.inv(covariance)), (x - mean)) / 2)
 
-
-def get_near_psd(A):
-    C = (A + A.T) / 2
-    eigval, eigvec = np.linalg.eig(C)
-    eigval[eigval < 0] = 0
-
-    return eigvec.dot(np.diag(eigval)).dot(eigvec.T)
-
